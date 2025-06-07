c47bc63373e9340477ca9a3f566a9606bfe7bedc
RobertTarpley <131493065+RobertTarpley@users.noreply.github.com>
2025-06-07 18:43:59 -0400

    adjusted fps

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,6 +23,10 @@ def main():
         
         #refresh the display
         pygame.display.flip()
+        
+        #cap the frame rate at 60 fps and set dt 
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
