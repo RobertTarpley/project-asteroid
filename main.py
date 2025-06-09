a858fa51add5ffe3a70d3e417cd9fbcb57e592db
RobertTarpley <131493065+RobertTarpley@users.noreply.github.com>
2025-06-09 19:12:17 -0400

    added player.py and play screen to main.py
    f

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -13,6 +14,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(x = SCREEN_WIDTH / 2 , y = SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,12 +25,13 @@ def main():
         #fill the screen with black
         screen.fill((0, 0, 0))
         
+        player.draw(screen)
+
         #refresh the display
         pygame.display.flip()
         
         #cap the frame rate at 60 fps and set dt 
         dt = clock.tick(60) / 1000
-        
 
 if __name__ == "__main__":
     main()
